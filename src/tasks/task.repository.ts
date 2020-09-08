67120d1d06ca38cd858ae8049b5dfa72aeb136a6
ricky <whdtjr2792@gmail.com>
2020-09-09 02:12:41 +0900

    typeorm

---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,6 @@
+import { EntityRepository, Repository } from 'typeorm/index';
+import { Task } from './task.entity';
+
+@EntityRepository(Task)
+export class TaskRepository extends Repository<Task> {
+}
