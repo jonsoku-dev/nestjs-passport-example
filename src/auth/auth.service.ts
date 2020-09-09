4c3a87909b75b0cef1f5aeafdf7e9aab3e13f17a
ricky <whdtjr2792@gmail.com>
2020-09-09 11:57:31 +0900

    signup 코드짜는 흐름

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
-    private userRepository: UserRepository
+    private userRepository: UserRepository,
   ) {
+  }
+
+  async signup(authCredentialDto: AuthCredentialsDto):Promise<void> {
+    return this.userRepository.signup(authCredentialDto);
   }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,16 @@
 import { EntityRepository, Repository } from 'typeorm/index';
 import { User } from './auth.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signup(authCredentialDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    await user.save();
+  }
+}
